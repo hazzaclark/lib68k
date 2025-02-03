139edf1e5eebdf7e558fe6bcf14e104c6ef86675
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-02-03 18:57:59 +0000

    updated sample

--- hello.asm
+++ hello.asm
@@ -1,22 +1,26 @@
 ;----------------------------------------------------------------------------------
-;			COPYRIGHT (C) HARRY CLARK 2025
+;                       COPYRIGHT (C) HARRY CLARK 2025
 ;----------------------------------------------------------------------------------
-;			LIB68K HELLO WORLD SAMPLE
+;                       LIB68K HELLO WORLD SAMPLE
 ;----------------------------------------------------------------------------------
 
-ORG			$1000
+    ORG     $0000           
+    DC.L    $FFFFFE         
+    DC.L    START          
+
+    ORG     $1000          
 
 MESSAGE:
-	DC.B		'HELLO LIB68K', 0
+    DC.B    'HELLO LIB68K', 0
 
 START:
-	LEA		MESSAGE, A0
-	MOVE.B		#9, D0
-	TRAP		#15
+    LEA     MESSAGE, A0
+    MOVE.B  #9, D0
+    TRAP    #15
 
-	MOVE.B		#0, D0
-	TRAP		#15
-	BRA		END
+    MOVE.B  #0, D0
+    TRAP    #15
+    BRA     END
 
 END:
-	RTS
+    RTS
