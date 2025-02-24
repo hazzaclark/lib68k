2b8c67434fa2c1e0bff357d701f031e3f7143c10
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-02-24 21:17:50 +0000

    Update 68KCONF.h

--- inc/68KCONF.h
+++ inc/68KCONF.h
@@ -69,6 +69,7 @@ void M68K_SET_INSTR_CALLBACK(unsigned CALLBACK, unsigned PC);
 void M68K_SET_MOVE_IRQ_INT(void);
 U16 M68K_FETCH_INSTR();
 U32 M68K_FETCH_LONG();
+int M68K_EXECUTE_INSTRUCTION(U16 INSTRUCTION);
 
 /*===============================================================================*/
 /*							68000 READ AND WRITE							     */
