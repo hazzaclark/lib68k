0acd42378ef2c73e7cdf7f985385ce28c82dd94d
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-03-29 18:28:55 +0000

    Update 68KCONF.h

--- inc/68KCONF.h
+++ inc/68KCONF.h
@@ -65,11 +65,8 @@
 
 #endif
 
-#if M68K_USE_ENDIANESS
-	#define	USE_BE M68K_OPT_OFF
-#else
-	#define	USE_LE 	M68K_OPT_ON
-#endif
+#define	USE_BE 		M68K_OPT_OFF
+#define	USE_LE 		M68K_OPT_ON
 
 #define 	M68K_MASK_OUT_ABOVE_8(A)            ((A) & 0xFF)
 #define 	M68K_MASK_OUT_ABOVE_16(A)           ((A) & 0xFFFF)
