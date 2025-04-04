360ff63be9ad2b1bdd7a2ad73da905f97b01fa77
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-04-04 16:44:37 +0100

    added imm sigs

--- inc/68KMEM.h
+++ inc/68KMEM.h
@@ -89,6 +89,10 @@ void M68K_WRITE_MEMORY_8(unsigned int ADDRESS, U8 VALUE);
 void M68K_WRITE_MEMORY_16(unsigned int ADDRESS, U16 VALUE);
 void M68K_WRITE_MEMORY_32(unsigned int ADDRESS, U32 VALUE);
 
+unsigned int M68K_READ_IMM_16(unsigned int ADDRESS);
+unsigned int M68K_READ_IMM_32(unsigned int ADDRESS);
+
+
 extern U8 ENABLED_FLAGS;
 
 #endif
