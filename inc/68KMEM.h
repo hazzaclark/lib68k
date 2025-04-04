5a0145ae66126285a6772ff088c97ea0ee8512e9
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-04-04 14:41:50 +0100

    couldve sworn i removed the definer

--- inc/68KMEM.h
+++ inc/68KMEM.h
@@ -89,7 +89,6 @@ void M68K_WRITE_MEMORY_8(unsigned int ADDRESS, U8 VALUE);
 void M68K_WRITE_MEMORY_16(unsigned int ADDRESS, U16 VALUE);
 void M68K_WRITE_MEMORY_32(unsigned int ADDRESS, U32 VALUE);
 
-extern U8 ENABLED_FLAGS = M68K_OPT_FLAGS;
-
+extern U8 ENABLED_FLAGS;
 
 #endif
