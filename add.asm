5615e315d13ac3534d970103c74121a7164f9272
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-04-10 16:50:38 +0100

    skips over RESET anyway

--- add.asm
+++ add.asm
@@ -1,11 +1,8 @@
     ORG $1000          
 
 START:
-    MOVE.L #5, D0       
-    MOVE.L #10, D1      
-    ADD.L D1, D0        
-    MOVE.L D0, $2000    
-
-    STOP #$2700        
-
-END
+        MOVE.L #5, D0       
+        MOVE.L #10, D1      
+        ADD.L D1, D0        
+        MOVE.L D0, $2000
+        STOP #$2700        
