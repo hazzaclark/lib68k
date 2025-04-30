095f14df1fbdb8dbaba3503260daadcb71bd245a
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-04-30 16:17:52 +0100

    fixed read for change in FLAG_N
    Rather than reading and write from the EA, we can do an imm read based on the result being attibuted to the Z FLAG

--- src/68KOPCODE.c
+++ src/68KOPCODE.c
@@ -1547,79 +1547,46 @@ M68K_MAKE_OPCODE(NBCD, 8, D, 0)
 M68K_MAKE_OPCODE(NEG, 8, D, 0)
 {
     unsigned* DEST = &M68K_DATA_HIGH;
-    unsigned EA = (U8)M68K_EA();
-    unsigned RESULT = 0 - M68K_MASK_OUT_ABOVE_8(*DEST);
-    unsigned SRC = M68K_READ_8(EA);
+    unsigned RESULT = M68K_MASK_OUT_ABOVE_8(*DEST);
 
-    M68K_FLAG_N = M68K_READ_8(RESULT);
-    M68K_FLAG_C = M68K_FLAG_X = M68K_READ_8(RESULT);
+    M68K_FLAG_N = READ_IMM_8();
+    M68K_FLAG_C = M68K_FLAG_X = READ_IMM_8();
     M68K_FLAG_V = *DEST & RESULT;
     M68K_FLAG_Z = M68K_MASK_OUT_ABOVE_8(RESULT);
 
-    *DEST = M68K_MASK_OUT_ABOVE_8(*DEST) | M68K_FLAG_Z;
+    *DEST = *DEST | M68K_FLAG_Z;
 
-    #ifndef USE_ADDRESSING
-
-    M68K_FLAG_N = M68K_READ_8(RESULT);
-    M68K_FLAG_C = M68K_FLAG_X = M68K_READ_8(RESULT);
-    M68K_FLAG_V = SRC & RESULT;
-    M68K_FLAG_Z = M68K_MASK_OUT_ABOVE_8(RESULT);
-
-    M68K_WRITE_8(EA, M68K_FLAG_Z);
-
-    #endif
+    M68K_WRITE_8(RESULT, M68K_FLAG_Z);
 }
 
 M68K_MAKE_OPCODE(NEG, 16, D, 0)
 {
     unsigned* DEST = &M68K_DATA_HIGH;
-    unsigned EA = (U16)M68K_EA();
-    unsigned RESULT = 0 - M68K_MASK_OUT_ABOVE_16(*DEST);
-    unsigned SRC = M68K_READ_16(EA);
+    unsigned RESULT = M68K_MASK_OUT_ABOVE_16(*DEST);
 
-    M68K_FLAG_N = M68K_READ_16(RESULT);
-    M68K_FLAG_C = M68K_FLAG_X = M68K_READ_16(RESULT);
+    M68K_FLAG_N = READ_IMM_16();
+    M68K_FLAG_C = M68K_FLAG_X = READ_IMM_16();
     M68K_FLAG_V = *DEST & RESULT;
     M68K_FLAG_Z = M68K_MASK_OUT_ABOVE_16(RESULT);
 
-    *DEST = M68K_MASK_OUT_ABOVE_16(*DEST) | M68K_FLAG_Z;
+    *DEST = *DEST | M68K_FLAG_Z;
 
-    #ifndef USE_ADDRESSING
-
-    M68K_FLAG_N = M68K_READ_16(RESULT);
-    M68K_FLAG_C = M68K_FLAG_X = M68K_READ_16(RESULT);
-    M68K_FLAG_V = SRC & RESULT;
-    M68K_FLAG_Z = M68K_MASK_OUT_ABOVE_16(RESULT);
-
-    M68K_WRITE_16(EA, M68K_FLAG_Z);
-
-    #endif
+    M68K_WRITE_16(RESULT, M68K_FLAG_Z);
 }
 
 M68K_MAKE_OPCODE(NEG, 32, D, 0)
 {
     unsigned* DEST = &M68K_DATA_HIGH;
-    unsigned EA = (U32)M68K_EA();
-    unsigned RESULT = 0 - M68K_MASK_OUT_ABOVE_32(*DEST);
-    unsigned SRC = M68K_READ_32(EA);
+    unsigned RESULT = M68K_MASK_OUT_ABOVE_32(*DEST);
 
-    M68K_FLAG_N = M68K_READ_32(RESULT);
-    M68K_FLAG_C = M68K_FLAG_X = M68K_READ_32(RESULT);
+    M68K_FLAG_N = READ_IMM_32();
+    M68K_FLAG_C = M68K_FLAG_X = READ_IMM_32();
     M68K_FLAG_V = *DEST & RESULT;
     M68K_FLAG_Z = M68K_MASK_OUT_ABOVE_32(RESULT);
 
-    *DEST = M68K_MASK_OUT_ABOVE_32(*DEST) | M68K_FLAG_Z;
+    *DEST = *DEST | M68K_FLAG_Z;
 
-    #ifndef USE_ADDRESSING
-
-    M68K_FLAG_N = M68K_READ_32(RESULT);
-    M68K_FLAG_C = M68K_FLAG_X = M68K_READ_32(RESULT);
-    M68K_FLAG_V = SRC & RESULT;
-    M68K_FLAG_Z = M68K_MASK_OUT_ABOVE_32(RESULT);
-
-    M68K_WRITE_32(EA, M68K_FLAG_Z);
-
-    #endif
+    M68K_WRITE_32(RESULT, M68K_FLAG_Z);
 }
 
 M68K_MAKE_OPCODE(NEGX, 8, DA, 0)
@@ -2375,10 +2342,10 @@ OPCODE_HANDLER M68K_OPCODE_HANDLER_TABLE[] =
     {MOVEQ_32_D_0,              0xF1C0,     0x7000,     4},  // MOVEQ #<data>,Dn
     {MULS_16_D_0,               0xF1C0,     0xC1C0,     70}, // MULS.W <ea>,Dn
     {MULU_16_D_0,               0xF1C0,     0xC0C0,     70}, // MULU.W <ea>,Dn
-    {NBCD_8_D_0,                0xF1F8,     0x4800,     6},  // NBCD <ea>
-    {NEG_8_D_0,                 0xF1F8,     0x4400,     4},  // NEG.B <ea>
-    {NEG_16_D_0,                0xF1F8,     0x4440,     4},  // NEG.W <ea>
-    {NEG_32_D_0,                0xF1F8,     0x4480,     6},  // NEG.L <ea>
+    {NBCD_8_D_0,                0xFFFF,     0x4839,     6},  // NBCD <ea>
+    {NEG_8_D_0,                 0xFFFF,     0x4439,     4},  // NEG.B <ea>
+    {NEG_16_D_0,                0xFFFF,     0x4479,     8},  // NEG.W <ea>
+    {NEG_32_D_0,                0xFFFF,     0x44B9,     12},  // NEG.L <ea>
     {NEGX_8_DA_0,               0xF1F8,     0x4000,     4},  // NEGX.B <ea>
     {NEGX_16_DA_0,              0xF1F8,     0x4040,     4},  // NEGX.W <ea>
     {NEGX_32_DA_0,              0xF1F8,     0x4080,     6},  // NEGX.L <ea>
