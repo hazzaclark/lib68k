b5d85e1594b18a71a0fb2b6253bdf9ed155838d4
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-05-10 15:33:48 +0100

    moved some masks around, and change the data range for IMM_32

--- src/68KOPCODE.c
+++ src/68KOPCODE.c
@@ -1301,7 +1301,7 @@ M68K_MAKE_OPCODE(MOVE, 16, IMM, D)
 
 M68K_MAKE_OPCODE(MOVE, 32, IMM, D)
 {
-    unsigned RESULT = M68K_DATA_LOW;
+    unsigned RESULT = M68K_DATA_HIGH;
     unsigned EA = READ_IMM_32();
 
     RESULT = EA;
@@ -2500,13 +2500,13 @@ OPCODE_HANDLER M68K_OPCODE_HANDLER_TABLE[] =
     {LSR_32_S_0,                0xF1F8,     0xE088,     8},  // LSR.L Dn,Dy
     {MOVE_8_D_0,                0xF1C0,     0x1000,     4},   // MOVE.B <ea>,Dn 
     {MOVE_16_D_0,               0xF1C0,     0x3000,     8},   // MOVE.W <ea>,Dn 
-    {MOVE_32_D_0,               0xF1C0,     0x2000,     12},   // MOVE.L <ea>,Dn
+    {MOVE_32_D_0,               0xF1C0,     0x2000,     20},   // MOVE.L <ea>,Dn
     {MOVE_8_D_D_0,              0xFFFF,     0x1200,     20},  // MOVE.B Dn,Dn
     {MOVE_16_D_D_0,             0xFFFF,     0x3200,     20},  // MOVE.W Dn,Dn
     {MOVE_32_D_D_0,             0xFFFF,     0x2200,     24},   // MOVE.L Dn,Dn
     {MOVE_8_IMM_D,              0xF1F8,     0x103C,     8},   // MOVE.W #<data>,Dn 
-    {MOVE_16_IMM_D,             0xF1FF,     0x303C,     8},   // MOVE.W #<data>,Dn 
-    {MOVE_32_IMM_D,             0xF1FF,     0x203C,     16},  // MOVE.L #<data>,Dn 
+    {MOVE_16_IMM_D,             0xF1F8,     0x303C,     8},   // MOVE.W #<data>,Dn 
+    {MOVE_32_IMM_D,             0xF1F8,     0x203C,     16},  // MOVE.L #<data>,Dn 
     {MOVE_32_D_POST_DEC,        0xFFFF,     0x2F00,     14},  // MOVE.L, Dn, -(SP)
     {MOVEA_16_DA_0,             0xF1C0,     0x203C,     4},  // MOVEA.W <ea>,An
     {MOVEA_32_DA_0,             0xF1C0,     0x203C,     4},  // MOVEA.L <ea>,An
