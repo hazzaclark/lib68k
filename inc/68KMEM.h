fd265149cc7a4fa708b22389e986dd4bd9d0eb3f
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-05-15 17:22:09 +0100

    best to keep the trace simplified
    looking at it, it is already too much as is

--- inc/68KMEM.h
+++ inc/68KMEM.h
@@ -18,17 +18,13 @@
 #define         M68K_OPT_VERB               (1 << 1)
 #define         M68K_OPT_DEVICE             (1 << 2)
 
-#define         M68K_OPT_FLAGS              (M68K_OPT_BASIC | M68K_OPT_VERB | M68K_OPT_PHASE_FETCH | M68K_OPT_PHASE_DATA | M68K_OPT_PHASE_EXEC)
+#define         M68K_OPT_FLAGS              (M68K_OPT_BASIC | M68K_OPT_VERB)
 
 #define         M68K_MAX_ADDR_START             0xFFFFFFF0
 #define         M68K_MAX_ADDR_END               0xFFFFFFFF
 
 #define         M68K_T0_SHIFT                   (1 << 3)
 #define         M68K_T1_SHIFT                   (1 << 4)
-
-#define         M68K_OPT_PHASE_FETCH            (1 << 4)  
-#define         M68K_OPT_PHASE_DATA             (1 << 5)  
-#define         M68K_OPT_PHASE_EXEC             (1 << 6)  
 
 typedef enum
 {
@@ -112,15 +108,6 @@ typedef struct
             (T1) ? ENABLE_TRACE_FLAG(M68K_T1_SHIFT) : DISABLE_TRACE_FLAG(M68K_T1_SHIFT); \
     } while(0)
 
-#if PHASE_HOOK == M68K_OPT_ON
-    #define PHASE_TRACE(PHASE, MSG, ...) \
-        do { \
-            if (IS_TRACE_ENABLED(M68K_OPT_VERB)) \
-                printf("[%s] " MSG "\n", PHASE, ##__VA_ARGS__); \
-        } while (0)
-#else
-    #define PHASE_TRACE(MSG, ...) ((void)0)
-#endif
 #endif
 
 unsigned int M68K_READ_MEMORY_8(unsigned int ADDRESS);
