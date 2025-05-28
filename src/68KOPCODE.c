abf453ca54f9c73f80a7e5e760c8bd004ab70d40
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-05-28 19:56:37 +0100

    added defs for ADDA PRE_DEC and POST_INC

--- src/68KOPCODE.c
+++ src/68KOPCODE.c
@@ -653,6 +653,38 @@ M68K_MAKE_OPCODE(ADDA, 32, AN, AY)
     DESTINATION = M68K_MASK_OUT_ABOVE_32(DESTINATION + SRC);
 }
 
+M68K_MAKE_OPCODE(ADDA, 16, PI, AY)
+{
+    int DESTINATION = M68K_ADDRESS_HIGH;
+    int SRC = ((U32)DESTINATION);
+
+    DESTINATION = M68K_MASK_OUT_ABOVE_16(DESTINATION + SRC);
+}
+
+M68K_MAKE_OPCODE(ADDA, 32, PI, AY)
+{
+    int DESTINATION = M68K_ADDRESS_HIGH;
+    int SRC = ((U32)DESTINATION);
+
+    DESTINATION = M68K_MASK_OUT_ABOVE_32(DESTINATION + SRC);
+}
+
+M68K_MAKE_OPCODE(ADDA, 16, PD, AY)
+{
+    int DESTINATION = M68K_ADDRESS_HIGH;
+    int SRC = ((U32)DESTINATION);
+
+    DESTINATION = M68K_MASK_OUT_ABOVE_16(DESTINATION + SRC);
+}
+
+M68K_MAKE_OPCODE(ADDA, 32, PD, AY)
+{
+    int DESTINATION = M68K_ADDRESS_HIGH;
+    int SRC = ((U32)DESTINATION);
+
+    DESTINATION = M68K_MASK_OUT_ABOVE_32(DESTINATION + SRC);
+}
+
 M68K_MAKE_OPCODE(ADDI, 8, IMM, 0)
 {
     int DESTINATION = M68K_DATA_LOW;
@@ -3675,6 +3707,10 @@ OPCODE_HANDLER M68K_OPCODE_HANDLER_TABLE[] =
     {ADDA_32_EA_0,              0xF1C0,     0xD1C0,     12},  // ADDA.L <ea>,An
     {ADDA_16_AN_AY,             0xF1F8,     0xD0C8,     8},  // ADDA.W An, Ay
     {ADDA_32_AN_AY,             0xF1F8,     0xD1C8,     8},  // ADDA.W An, Ay
+    {ADDA_16_PD_AY,             0xF1F8,     0xD0E0,     14},  // ADDA.W -(An), Ay
+    {ADDA_32_PD_AY,             0xF1F8,     0xD1E0,     16},  // ADDA.L -(An), Ay
+    {ADDA_16_PI_AY,             0xF1F8,     0xD0D8,     12},  // ADDA.W (An)+, Ay
+    {ADDA_32_PI_AY,             0xF1F8,     0xD1D8,     16},  // ADDA.L (An)+, Ay
     {ADDI_8_IMM_0,              0xFFF8,     0x0600,     8},  // ADDI.B #<data>,<ea>
     {ADDI_16_IMM_0,             0xFFF8,     0x0640,     8},  // ADDI.W #<data>,<ea>
     {ADDI_32_IMM_0,             0xFFF8,     0x0680,     16}, // ADDI.L #<data>,<ea>
