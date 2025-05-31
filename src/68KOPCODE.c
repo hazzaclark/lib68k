87c1a0c9ba439317b21d1b1ff9462d54ccf4e7c4
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-05-31 21:30:41 +0100

    Update 68KOPCODE.c

--- src/68KOPCODE.c
+++ src/68KOPCODE.c
@@ -3852,9 +3852,9 @@ OPCODE_HANDLER M68K_OPCODE_HANDLER_TABLE[] =
     {ADD_32_D_EA_PRE_DEC,       0xF1F8,     0xD1A0,     8},  // ADD.L Dn, -(Ay)
     {ADD_16_PRE_DEC_D,          0xF1C0,     0xD068,     12}, // ADD.W -(An), Dy 
     {ADD_32_PRE_DEC_D,          0xF1F8,     0xD0A0,     12}, // ADD.L -(An), Dy 
-    {ADD_8_A_AI,                0xFFF8,     0xD028,     8},  // ADD.B $imm(An), Dy
-    {ADD_16_A_AI,               0xFFF8,     0xD068,     8},  // ADD.W $imm(An), Dy
-    {ADD_32_A_AI,               0xFFF8,     0xD0A8,     10},  // ADD.L $imm(An), Dy
+    {ADD_8_A_AI,                0xF1F8,     0xD028,     8},  // ADD.B $imm(An), Dy
+    {ADD_16_A_AI,               0xF1F8,     0xD068,     8},  // ADD.W $imm(An), Dy
+    {ADD_32_A_AI,               0xF1F8,     0xD0A8,     12},  // ADD.L $imm(An), Dy
     {ADDA_16_EA_0,              0xF1C0,     0xD0C0,     8},  // ADDA.W <ea>,An
     {ADDA_32_EA_0,              0xF1C0,     0xD1C0,     12},  // ADDA.L <ea>,An
     {ADDA_16_AN_AY,             0xF1F8,     0xD0C8,     8},  // ADDA.W An, Ay
