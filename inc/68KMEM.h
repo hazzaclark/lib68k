87431d9a4c6a53b3de1e5575657e09a9548b96c0
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-08-11 18:22:05 +0100

    size handlers was too limiting

--- inc/68KMEM.h
+++ inc/68KMEM.h
@@ -99,11 +99,11 @@ typedef struct
 #define         JUMP_HOOK                       M68K_OPT_ON
 #define         PHASE_HOOK                      M68K_OPT_ON
 
-#if MEM_TRACE_HOOK == M68K_OPT_OFF
+#if MEM_TRACE_HOOK == M68K_OPT_ON
     #define MEM_TRACE(OP, ADDR, SIZE, VAL) \
         do { \
             if (IS_TRACE_ENABLED(M68K_OPT_BASIC) && CHECK_TRACE_CONDITION()) \
-                printf("[TRACE] %c -> ADDR:0x%0X SIZE:%d VALUE:0x%04X\n", \
+                printf("[TRACE] %c -> ADDR:0x%08X SIZE:%d VALUE:0x%08X\n", \
                       (char)(OP), (ADDR), (SIZE), (VAL)); \
         } while(0)
 #else
