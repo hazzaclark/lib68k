b4c7ac057a11ae486db2eb87d7c720039db6c3f5
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-08-20 18:10:26 +0100

    moved file loading to conf
    also removed redundancy methods that are otherwise handled in conf through file loading anyway

--- src/main.c
+++ src/main.c
@@ -30,14 +30,7 @@ int main(int argc, char** argv)
     M68K_INIT();
     SHOW_MEMORY_MAPS();
 
-    printf("LOADING BINARY FILE: %s AT 0x%04X\n", argv[1], M68K_REG_PC);
-
     int FILE_SIZE = LOAD_BINARY_FILE(argv[1], M68K_REG_PC);
-    if(FILE_SIZE < 0)
-    {
-        printf("FAILED TO LOAD BINARY FILE\n");
-        return 1;
-    }
 
     M68K_PULSE_RESET();
 
