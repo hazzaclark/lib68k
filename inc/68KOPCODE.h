f84eed6da104a98d84ca44df26e6a41bf6b350a8
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-10-13 18:23:57 +0100

    cleaned up opcode header
    majority of the stuff ended up going unused and or is defined in a similar vein elsewhere
    
    majority of the stuff pertaining towards the second opcode struct was to do with being able to test out the efficiency of reading files and discerning between directives and opcodes and the like (which is no longer needed)

--- inc/68KOPCODE.h
+++ inc/68KOPCODE.h
@@ -14,14 +14,6 @@
 #include "68KCONF.h"
 #include "common.h"
 
-/* SYSTEM INCLUDES */
-
-#include <malloc.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
-
 #if defined(OPERAND_ACCESS)
     #define OPERAND_ACCESS
     #else
@@ -42,30 +34,10 @@
     #else
     #define BUILD_OP_TABLE
 
-    #define         USE_OPCODE_DEBUG                M68K_OPT_OFF
-
-    #define         OPCODE_ILLEGAL_MASK             (0x00000 << 16)
-    #define         OPCODE_DEF_MASK                 (0xFF00 >> 0x0000)
-    #define         OPCODE_BYTE_MASK                (0xF1F8 >> 0x0000)
+    #define         USE_OPCODE_DEBUG                M68K_OPT_ON
 
     #define         OPCODE_BIT_MASK                 0xFF
     #define         OPCODE_MAX                      0x10000
-
-    typedef struct OPCODE
-    {
-        char NAME;
-        char SIZE;
-        char BITS;
-        void(*HANDLER)(void);
-        unsigned MASK;
-        char CYCLES;
-
-        char PROCESS;
-        char EA;
-
-    } OPCODE;
-
-    #ifndef USE_OPCODE_HANDLER_TABLE
 
     typedef struct OPCODE_HANDLER
     {
@@ -76,36 +48,19 @@
 
     } OPCODE_HANDLER;
 
-    #endif
-
-    // THE WAY THAT I ORIGINALLY INTENDED FOR THIS MACRO TO
-    // BE LAID OUT WAS THAT "REG" WOULD ENCOMPASS THE BASELINE
-    // INITIALISER (HENCE WHY MOST OF THE DEFS HAVE "0" IN PLACE OF REG)
-
-    // THE REST OF THE NAMES ARE INDICATIVE OF THE PRE-PROC FILL-INS
+    // ALL ENCOMPASSING MACRO FOR CREATING OPCODES
+    // CONCATENATES THE RESPECTIVE TYPES ACCORDINGLY
+    //
+    // THE PARAMETERS THAT ARE CONCATENATED ARE
+    // VERBATIM AS THEY APPEAR IN THEIR INSTRUCTION
+    // FORMAT IN RELEVANT DOCUMENTATION 
 
     #define M68K_MAKE_OPCODE(OP, SIZE, MODE, REG) \
     void OP##_##SIZE##_##MODE##_##REG(void)
 
-    #define     OPCODE_NAME         OPCODE_BASE.NAME      
-    #define     OPCODE_SIZE         OPCODE_BASE.SIZE
-    #define     OPCODE_BITS         OPCODE_BASE.BITS
-    #define     OPCODE_PROCESS      OPCODE_BASE.PROCESS
-    #define     OPCODE_EA           OPCODE_BASE.EA
-    #define     OPCODE_HANDLE       OPCODE_BASE.HANDLER
-    #define     OPCODE_CYCLES       OPCODE_BASE.CYCLES
-
 void M68K_OP_1010(void);
 void M68K_OP_1111(void);
 void M68K_OP_ILLEGAL_EX(void);
-
-int EXTRACT_OPCODE(char* SRC, char* NAME, int* SIZE);
-int CHECK_OPCODE_LENGTH(char* SRC, char* DEST, int MAX);
-int GET_OP_CYCLES(OPCODE* OPCODE_BASE, int EA_MODE, int* CPU_TYPE);
-OPCODE* FIND_OPCODE(char* NAME, int SIZE);
-
-extern OPCODE OPCODE_BASE;
-extern OPCODE_HANDLER OP_HANDLE;
 
 extern OPCODE_HANDLER M68K_OPCODE_HANDLER_TABLE[OPCODE_MAX];
 extern void(*M68K_OPCODE_JUMP_TABLE[0x10000])(void);
